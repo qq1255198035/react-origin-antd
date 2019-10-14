786cea6de493a164c6fb0dd214420cb6bfd82c01
wangz <m17604433774@163.com>
2019-10-14 14:55:26 +0800

    test1

---
+++ src/view/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="Home">
+            <h1>
+                hello react
+            </h1>
+        </div>
+    );
+}
+
+export default Home;
