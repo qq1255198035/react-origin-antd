2434f9aabbcb722f51ebebd45067fe1a07ea471d
wangz <m17604433774@163.com>
2019-12-04 16:09:23 +0800

    12-4

--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button } from 'antd';
 import './Home.css';
 
-function Home() {
-    return (
-        <div className="Home">
-            <h1>
-                hello react
-            </h1>
-        </div>
-    );
+class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state = {isToggleOn: true};
+    }
+
+    render() {
+        return(
+            <div className="Home">
+                <h1>
+                    hello react
+                </h1>
+                <Button type="primary" onClick={() => this.handleClick()}>
+                    {this.state.isToggleOn ? 'On' : 'Off'}
+                </Button>
+            </div>
+        )
+    }
+
+    handleClick(){
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }))
+    }
 }
 
 export default Home;
